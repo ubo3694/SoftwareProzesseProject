7a47e6f8f1859dc8fdf0a9b0f18ee0c9cebb3e13
Ugurcan Borihan <ugurcan.borihan@hotmail.com>
2023-09-29 19:47:08 +0200

    fehler eingbeaut 7

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -37,7 +37,7 @@ namespace UnitTest
 
             var model = result.ViewData.Model as List<Training>;
             // Assert.Equal(2, model.Count);
-            Assert.Equal(6, model.Count); //Fehler
+            Assert.Equal(7, model.Count); //Fehler
         }
 
         [Fact]
